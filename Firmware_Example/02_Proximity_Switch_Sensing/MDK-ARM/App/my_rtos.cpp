d7bc0fa8e8fae1fbc08743dbba4306f9d4aaabb4
JM.Hugo314 <12089619+jmhugo314@user.noreply.gitee.com>
2026-04-21 21:21:25 +0800

    [feat]:complete lab02 proximity_switch

---
+++ Firmware_Example/02_Proximity_Switch_Sensing/MDK-ARM/App/my_rtos.cpp
@@ -0,0 +1,120 @@
+/**
+  ******************************************************************************
+  * @file           :
+  * @author         : Xiang Guo
+  * @brief          : 
+  * @date	          : 2023/05/07
+  ******************************************************************************
+  * @attention
+  *
+  *
+  ******************************************************************************
+  */
+
+/* ------------------------------ Includes ------------------------------ */
+
+#include "my_rtos.h"
+#include "my_config.h"
+/* ------------------------------ Defines ------------------------------ */
+
+/* ------------------------------ Variables ------------------------------ */
+
+/* ------------------------------ Functions ------------------------------ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void My_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim);
+
+void StartDefaultTask(void *argument);
+void StartDebugTask(void *argument);
+void StartKeyScanTask(void *argument);
+#ifdef __cplusplus
+}
+#endif
+
+/* ------------------------------ Interrupts ------------------------------ */
+
+void My_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  if (htim->Instance == g_linearModule[0].stepper.p_htim->Instance)
+  {
+    g_linearModule[0].ControlLoop();
+  }
+  else if (htim->Instance == g_linearModule[1].stepper.p_htim->Instance)
+  {
+    g_linearModule[1].ControlLoop();
+  }
+}
+
+/* ------------------------------ Tasks ------------------------------ */
+
+/**
+  * @brief  .
+  * @author Xiang Guo
+  * @param  none
+  * @retval none
+  */
+void StartDefaultTask(void *argument)
+{
+  g_linearModule[0].MotionConfig(1, 10.0f, 500.0f);
+  g_linearModule[1].MotionConfig(1, 10.0f, 500.0f);
+  g_linearModule[0].SetMode(x_linear_module::MODULE_MODE_VELOCITY);
+  g_linearModule[1].SetMode(x_linear_module::MODULE_MODE_VELOCITY);
+  osThreadResume(debugTaskHandle);
+  osThreadResume(keyScanTaskHandle);
+  osThreadSuspend(defaultTaskHandle);
+  /* Infinite loop */
+  for (;;)
+  {}
+}
+
+/**
+  * @brief  .
+  * @param  none
+  * @retval none
+  */
+void StartDebugTask(void *argument)
+{
+  for (;;)
+  { 
+    if (g_key[0].released()) // find home position
+    {
+     g_linearModule[0].SetTargetVelocity(-10.0f);
+     g_linearModule[1].SetTargetVelocity(-10.0f);
+    }
+    if (g_key[1].released())
+    {
+//      g_linearModule[1].SetTargetVelocity(-10.0f);
+      g_linearModule[0].SetTargetVelocityHard(0.0f);
+      g_linearModule[1].SetTargetVelocityHard(0.0f);
+    }
+    if (g_key[2].released())
+    {
+      g_linearModule[0].SetTargetVelocity(10.0f);
+    }
+    if (g_key[3].released())
+    {
+      g_linearModule[1].SetTargetVelocity(10.0f);
+    }
+    osDelay(10);
+  }
+}
+
+/**
+  * @brief  按键扫描任务，50ms扫描一次按键，更新按键状态，按键按下时，更新按键状态为按下，按键释放时，更新按键状态为释放
+  * @param  none
+  * @retval none
+  */
+void StartKeyScanTask(void *argument)
+{
+  for (;;)
+  {
+    for (uint8_t i = 0; i < 4; i++)
+    {
+      g_key[i].update();
+    }
+    osDelay(50);
+  }
+}
